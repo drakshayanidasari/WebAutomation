340d1f73348c2c4986dda4f99a1806b7489e7954
sdds <asdwfdff@example.com>
2025-08-07 13:54:40 +0530

    new file

---
+++ src/test/java/TestGit.java
@@ -0,0 +1,12 @@
+
+public class TestGit {
+//	=====================================
+	public static void main(String args[]) {
+		
+	
+            String str="ja@va$ is pr123ogra^78mming la423425ngu@!age";
+           String with= str.replaceAll("[^a-z,A-Z,0-9]", "");
+            System.out.println("print without special character====:"+" "+with);
+
+	}
+}
